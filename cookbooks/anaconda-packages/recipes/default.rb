662dff70d23d36b2ee60c33e4f7ee204916a689f
nareynolds <nathaniel.reynolds@gmail.com>
2015-05-27 12:16:04 -0400

    fix package install example of rpy2

--- cookbooks/anaconda-packages/recipes/default.rb
+++ cookbooks/anaconda-packages/recipes/default.rb
@@ -7,14 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Example: install rpy2 into Anaconda
-anaconda_package 'rpy2' do
-  action :install
-end
-
 # Example: install django into Anaconda
 anaconda_package 'django' do
   action :install
+end
+
+# Example: install rpy2 into Anaconda
+anaconda_package 'rpy2' do
+  action :install
+  channel "https://conda.binstar.org/r"
 end
 
 # change and/or repeat as needed...
